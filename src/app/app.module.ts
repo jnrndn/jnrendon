b8a22bd97a78d119b9c366c83c870ef4c6a79f21
Juan Jose Rendon <juan.rendon@yuxiglobal.com>
2019-01-07 16:28:50 -0500

    More setup: add firebase and angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { firebaseConfig } from '../environments/firebase.config';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
@@ -13,6 +15,7 @@ import { MaterialModule } from './material/material.module';
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [ AppComponent ]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB7q8nANU9tPMI5mvsSTjDcDWBgWjO73iY',
+  authDomain: 'juanjo-web-page.firebaseapp.com',
+  databaseURL: 'https://juanjo-web-page.firebaseio.com',
+  projectId: 'juanjo-web-page',
+  storageBucket: 'juanjo-web-page.appspot.com',
+  messagingSenderId: '685902116692',
+};
